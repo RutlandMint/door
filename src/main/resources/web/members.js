648a8519176b69a4c44fbc44cb0254443af38e68
bkuker <bkuker@martellotech.com>
2018-03-13 12:41:33 -0400

    Better table in member list

--- src/main/resources/web/members.js
+++ src/main/resources/web/members.js
@@ -6,7 +6,7 @@ $(function(){
 	}).then(function(res) {
 		var $mb = $("#js-memberList");
 		$mb.empty();
-		var _tLogRow = _.template("<tr class='<%-cl%>'><td><%-name%></td><td><%-level%></td><td><%-status%></td><td><%-signedWaiver%></td><td><%-signedAgreement%></td><td><%-denyMessage%></td></tr>");
+		var _tLogRow = _.template("<tr class='<%-cl%>'><td><%-name%></td><td><%-keyCardNumber%></td><td><%-level%></td><td><%-status%></td><td><%-signedWaiver%></td><td><%-signedAgreement%></td><td><%-denyMessage%></td></tr>");
 		_.each(res.members, function(entry) {
 			if ( entry.accessGranted ){
 				entry.denyMessage = "";
@@ -14,9 +14,16 @@ $(function(){
 			} else {
 				entry.cl = "table-danger";
 			}
+			if ( !entry.keyCardNumber ){
+				entry.cl = "table-warning";
+				entry.denyMessage = "No Card";
+			}
 			entry.signedWaiver = entry.signedWaiver?"":"Missing";
 			entry.signedAgreement = entry.signedAgreement?"":"Missing";
 			$mb.prepend(_tLogRow(entry));
 		});
+		 $('#js-memberTable').DataTable({
+			 "paging":   false
+		 });
 	});
 });
